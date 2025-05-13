4329b9e836afd51204f870128b7bb10b38e91d73
Boy Aditya <boy.adityarr@gmail.com>
2025-05-13 19:06:21 +0700

    feat(map): Add base map, sticky styling

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,12 +1,15 @@
 import SearchBar from "@/components/SearchBar.jsx";
 import React from "react";
 import DestinationCard from "@/components/DestinationCard.jsx";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 
 const Destination = () => {
   return (
-    <div className="w-full max-w-10/12 mx-auto">
+    <div className="mt-0.5 mx-auto w-full max-w-10/12">
       {/* Search Bar */}
-      <section className="my-8">
+      <section className="sticky top-0 z-10 bg-neutral-bg py-6">
         <SearchBar />
       </section>
 
@@ -17,13 +20,28 @@ const Destination = () => {
           <DestinationCard variant="row" />
           <DestinationCard variant="row" />
           <DestinationCard variant="row" />
+          <DestinationCard variant="row" />
+          <DestinationCard variant="row" />
+          <DestinationCard variant="row" />
         </div>
 
         {/* Map */}
-        <div className="col-span-1 h-[500px] rounded-lg bg-amber-400 shadow-md">
-          <p className="flex h-full items-center justify-center text-lg font-medium text-white">
-            Peta
-          </p>
+        <div className="col-span-1 sticky top-28 h-[calc(100vh-1rem)]">
+          <MapContainer
+            center={[-7.5675595,110.7954161]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-7.5675595,110.7954161]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </section>
     </div>
